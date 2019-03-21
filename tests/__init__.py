97bcbc493274a73cfa7ab3c47b8b73510a37dc77
Luciano Rossi <lukio@gcoop.coop>
2019-03-21 18:08:14 -0300

    Update tests

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+try:
+    from trytond.modules.sale_invoice_line_create_wizard.tests.test_sale_invoice_line_create_wizard import suite
+except ImportError:
+    from .tests import suite
+
+__all__ = ['suite']
