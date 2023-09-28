aab387a83db5ac2d16baa32430a1c284d16b84f8
Adrián Bernardi <adrian.bernardi@gmail.com>
2023-09-28 09:03:59 -0300

    Use set for depends. Leave only the necessary.

--- sale.py
+++ sale.py
@@ -1,14 +1,17 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part of the sale_invoice_line_create_wizard module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 
 
 class SaleLine(metaclass=PoolMeta):
     __name__ = 'sale.line'
 
-    sale_party = fields.Function(fields.Many2One('party.party', 'Party'),
+    sale_party = fields.Function(fields.Many2One('party.party', 'Party',
+        context={'company': Eval('company', -1)}, depends={'company'}),
         'get_sale_party', searcher='search_sale_party')
 
     def get_sale_party(line, name):
